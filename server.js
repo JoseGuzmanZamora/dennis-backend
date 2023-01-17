9a8d011b71984d1514500817af34968a9f98e8bb
JoseGuzmanZamora <joseguzmanzamora@gmail.com>
2023-01-16 20:42:35 -0600

    Added fly.io configuration for deployment and grabbed sample code for open ai initialization

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Dennis content generation app is running in port ${port}`)
 })
