9c498b12daba5bcd445cddcd5ae2c9a19e5bfbaa
JoseGuzmanZamora <joseguzmanzamora@gmail.com>
2023-01-31 22:34:48 -0600

    Updated server to allow for cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import service from "./Main/open-service.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
 app.use(express.json())   
 
 const port = process.env.PORT || 3000;
